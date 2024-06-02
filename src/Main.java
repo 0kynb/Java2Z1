83c2729ad66681cc03a91eff5dd7e443c67189ea
First test <vitik03.90@gmail.com>
2024-06-02 23:38:57 +0300

    git@github.com:0kynb/Java2Z1.git

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTickets = 15323;
+        int priceMile = 20;
+        int freeMile = priceTickets/priceMile;
+        System.out.println("Итоговое количество бесплатных миль " +
+                freeMile);
+    }
+
+}
