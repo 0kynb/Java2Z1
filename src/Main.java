3015aa26a88d95ddfb8abb36c319cdb266520f14
0kynb <167920273+0kynb@users.noreply.github.com>
2024-06-04 22:37:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
         int priceTickets = 15323;
         int priceMile = 20;
-        int freeMile = priceTickets/priceMile;
-        System.out.println("Итоговое количество бесплатных миль " +
-                freeMile);
+        int freeMile = priceTickets / priceMile;
+        System.out.println("Итоговое количество бесплатных миль " + freeMile);
     }
-
 }
